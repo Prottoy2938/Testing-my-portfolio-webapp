84981f9701b9081e82419f4d016add09e8afdac1
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-01 23:23:34 +0600

    edited config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/Testing-my-portfolio-webapp",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
